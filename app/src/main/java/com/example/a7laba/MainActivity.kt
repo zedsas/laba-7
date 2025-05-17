cd0229f3b4208258e001d997373eb054712975d6
zedsas <tusyuk01@mail.ru>
2025-05-17 23:09:58 +0700

    1 Задание выполненно

--- app/src/main/java/com/example/a7laba/MainActivity.kt
+++ app/src/main/java/com/example/a7laba/MainActivity.kt
@@ -1,20 +1,123 @@
 package com.example.a7laba
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.yandex.mapkit.MapKitFactory
+import com.yandex.mapkit.geometry.Point
+import com.yandex.mapkit.map.CameraPosition
+import com.yandex.mapkit.mapview.MapView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mapView: MapView
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private val LOCATION_PERMISSION_REQUEST_CODE = 100
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        MapKitFactory.setApiKey("c01811d7-3f02-40f6-aa5a-6b50d39df857")
+        MapKitFactory.initialize(this)
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        mapView = findViewById(R.id.mapView)
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+
+        findViewById<Button>(R.id.btnZoomToKemerovo).apply {
+            text = "Почтамат"
+            setOnClickListener {
+                val cameraPosition = CameraPosition(
+                    Point(55.354993, 86.085805),
+                    15.0f,
+                    0.0f,
+                    0.0f
+                )
+                mapView.map.move(cameraPosition)
+            }
         }
+
+        findViewById<Button>(R.id.btnZoomToCurrentLocation).setOnClickListener {
+            checkLocationPermission()
+        }
+    }
+
+    private fun checkLocationPermission() {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            getCurrentLocation()
+        } else {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                LOCATION_PERMISSION_REQUEST_CODE
+            )
+        }
+    }
+
+    private fun getCurrentLocation() {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            fusedLocationClient.lastLocation.addOnSuccessListener { location ->
+                location?.let {
+                    val cameraPosition = CameraPosition(
+                        Point(it.latitude, it.longitude),
+                        15.0f,
+                        0.0f,
+                        0.0f
+                    )
+                    mapView.map.move(cameraPosition)
+                } ?: run {
+                    Toast.makeText(
+                        this,
+                        "Не удалось получить местоположение",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getCurrentLocation()
+            } else {
+                Toast.makeText(
+                    this,
+                    "Для работы функции необходимо разрешение",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+        MapKitFactory.getInstance().onStart()
+    }
+
+    override fun onStop() {
+        mapView.onStop()
+        MapKitFactory.getInstance().onStop()
+        super.onStop()
     }
 }
